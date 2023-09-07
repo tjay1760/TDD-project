a7e644f1dc42d8844dbd8351b42a4862061963a0
John Murianki <mcjthiongo@gmail.com>
2023-09-07 15:08:32 +0300

    add initial test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,2 +1,11 @@
+require_relative '../solver'
 describe Solver do
+    before :each do
+        @solver = Solver.new
+    end 
+    describe '#solver' do
+        it 'returns an instance of solver' do
+            @solver.should be_instance_of Solver
+        end
+    end
 end
