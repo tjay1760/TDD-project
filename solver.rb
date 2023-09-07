e9b9dfe4aff891e3d8885c773a814e03a4ae4f63
Oumaima Nejjari <oumanj1998@gmail.com>
2023-09-07 16:39:14 +0100

    Fixing linter errors

--- solver.rb
+++ solver.rb
@@ -1,29 +1,26 @@
 class Solver
-    def factorial(num)
-      factorial = 1 # Initialize factorial to 1, not 0
-      while num > 0 do
-        factorial *= num
-        num -= 1 # Use -= instead of --
-      end
-      factorial # Return the factorial value
+  def factorial(num)
+    factorial = 1 # Initialize factorial to 1, not 0
+    while num.positive?
+      factorial *= num
+      num -= 1 # Use -= instead of --
     end
-
-    def reversestring(word)
-    word.reverse
-    end
-
-      def fizzbuzz(num)
-        if num%3==0 && num%5== 0
-        'fizzbuzz'
-        elsif num% 3 == 0
-        'fizz'
-        elsif num% 5 == 0
-        'buzz'
-        else
-        num. to_s
-    end
-
-   
-end
+    factorial # Return the factorial value
   end
-  
+
+  def reversestring(word)
+    word.reverse
+  end
+
+  def fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
+    else
+      num.to_s
+    end
+  end
+end
