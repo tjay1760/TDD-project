d929283149b0a01184a77140e3747ba9dfd7004c
John Murianki <mcjthiongo@gmail.com>
2023-09-07 15:30:10 +0300

    add tests for factorials

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -10,4 +10,21 @@ describe Solver do
       expect(@solver).to be_an_instance_of(Solver)
     end
   end
+
+  describe '#factorial' do
+    it 'returns 1 for input 0' do
+      result = @solver.factorial(0)
+      expect(result).to eq(1)
+    end
+
+    it 'returns 1 for input 1' do
+      result = @solver.factorial(1)
+      expect(result).to eq(1)
+    end
+
+    it 'returns the correct factorial for a positive number' do
+      result = @solver.factorial(5)
+      expect(result).to eq(120)
+    end
+  end
 end
