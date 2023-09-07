8f98ab8a292871e6b0f937d75a3e2bcd2c79e64b
Oumaima Nejjari <oumanj1998@gmail.com>
2023-09-07 16:24:23 +0100

    Add reverse string code

--- solver.rb
+++ solver.rb
@@ -7,6 +7,21 @@ class Solver
       end
       factorial # Return the factorial value
     end
+
+    def reversestring(word)
+    word.reverse
+    end
+
+     def fizzbuzz(num)
+        if num%3==0 && num%5== 0
+        'fizzbuzz'
+        elsif num% 3 == 0
+        'fizz'
+        elsif num% 5 == 0
+        'buzz'
+        else
+        num. to_s
+    end
    
 end
   end
