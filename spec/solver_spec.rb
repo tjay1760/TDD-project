dee3c256a1255900b2388d5d585cf997c6704323
John Murianki <mcjthiongo@gmail.com>
2023-09-07 15:25:15 +0300

    Add initial intance test for solver

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,11 +1,13 @@
 require_relative '../solver'
+
 describe Solver do
-    before :each do
-        @solver = Solver.new
-    end 
-    describe '#solver' do
-        it 'returns an instance of solver' do
-            @solver.should be_instance_of Solver
-        end
+  before :each do
+    @solver = Solver.new
+  end
+
+  describe '#solver' do
+    it 'returns an instance of solver' do
+      expect(@solver).to be_an_instance_of(Solver)
     end
+  end
 end
